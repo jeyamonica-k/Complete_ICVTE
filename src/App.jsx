9b796f1b8f746c1da92a61c2500bd50d63a0c428
monicakodeeswaran32@gmail.com <monicakodeeswaran32@gmail.com>
2025-03-25 20:31:21 +0530

    Contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import  InstituteLogin from './Institute_login/InstituteLogin'
 import AdminLogin from "./Institute_login/AdminLogin"
 import MainCourse from "./Course_sec/MainCourse"
 import MainAbout from './About Us/MainAbout'
+import MainContact from './ContactUs/MainContact'
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       <Route path="/InstituteLogin" element={<InstituteLogin/>}></Route>
       <Route path="/Adminlogin" element={<AdminLogin/>}></Route>
       <Route path="/Course" element={<MainCourse/>}></Route>
-      <Route path="/" element={<MainAbout/>}></Route>
+      <Route path="/" element={<MainContact/>}></Route>
+      <Route path="/About" element={<MainAbout/>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
