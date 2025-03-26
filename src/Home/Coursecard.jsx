aa050ba3603385daaf9a9d10455ffa2fffeffdd6
monicakodeeswaran32@gmail.com <monicakodeeswaran32@gmail.com>
2025-03-26 07:16:59 +0530

    Act

--- src/Home/Coursecard.jsx
+++ src/Home/Coursecard.jsx
@@ -31,12 +31,12 @@ function Coursecard() {
 
               return(<>
         <div className=' m-5 rounded-md text-center  border-1 border-gray-300 sm:w-[80%]  lg:w-[25%] lg:h-[400px]  lg:rounded-2xl xl:w-[20%]
-        shadow-md transition-all duration-500 ease-in-out transform active:scale-105 hover:scale-105 active:shadow-[0px_10px_30px_rgba(0,0,0,0.7)] hover:shadow-[0px_10px_30px_rgba(0,0,0,0.7)]' >
+        shadow-md transition-all duration-500 ease-in-out transform active:scale-105 focus:scale-105 hover:scale-105 focus:shadow-[0px_10px_30px_rgba(0,0,0,0.7)] active:shadow-[0px_10px_30px_rgba(0,0,0,0.7)] hover:shadow-[0px_10px_30px_rgba(0,0,0,0.7)]' >
         <img src={course_images[index].src} alt="Affliation_img" className=' w-[100%] h-[50%] rounded-t-md  lg:rounded-t-2xl  ' />
         <h1 className='text-[#192C57] font-bold text-xl my-1 md:text-2xl p-1'>{course_images[index].title}</h1>
         <p className='my-1 text-sm text-gray-800 md:text-base p-1'>"{course_images[index].desc}"</p>
         <button className='bg-[#D94426] text-white font-medium border rounded-lg py-2 px-2.5 m-2 cursor-pointer
-         active:bg-[#900000] hover:bg-[#900000] md:mb-4'>Apply</button>
+         active:bg-[#900000] focus:bg-[#900000] hover:bg-[#900000] md:mb-4'>Apply</button>
          </div>
               </>)
 
